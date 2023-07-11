f4a0e4d714c61ebfe5731dd696ffcfcc31b2eae4
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-07-11 14:18:01 +0530

    final commit

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+
 	"github.com/shomali11/slacker"
 )
 
-func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent)  {
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticsChannel {
 		fmt.Println("Command Events")
 		fmt.Println(event.Timestamp)
@@ -20,21 +22,28 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent)  {
 	}
 }
 
-func main()  {
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5529699535137-5510423351942-ZjdDV2biRRFeQEcWljLKCcMc")
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A05FKLQ1GM7-5547745943014-c45fda0ee40a1517a69913d16138dcde17634dc8c846c6b565b8b40bb060d18e")
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "Your Bot Token")
+	os.Setenv("SLACK_APP_TOKEN", "Your App Token")
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
-	go printCommandEvents(bot.CommandEvent())
+	go printCommandEvents(bot.CommandEvents())
 
-	bot.Command("My yob is <year>", &slacker.CommandDefinition) {
+	bot.Command("My yob is <year>", &slacker.CommandDefinition{
 		Description: "yob calculator",
-		Example: "My yob is 2002",
+
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
-			
-		}
-	}
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				println("error")
+			}
+			age := 2023 - yob
+			r := fmt.Sprintf("Your age is %d", age)
+			response.Reply(r)
+		},
+	})
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
